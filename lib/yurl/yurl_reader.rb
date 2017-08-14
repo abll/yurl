217b796b8126d009e5fdf694332e3cc883de6b2c
abll <abellawal@yahoo.com>
2017-08-14 07:44:19 -0700

    Committing the work for the weekened

--- lib/yurl/yurl_reader.rb
+++ lib/yurl/yurl_reader.rb
@@ -1,31 +1,73 @@
-#Class File To Implement Yurl_Reader
-require "psych"
+# Class File To Implement Yurl_Reader
+require 'psych'
+require 'pp'
 
 module Yurl
     class YurlReader
         attr_accessor :secret_ruby, :file_name
 
-        def self.parse_file(file_name)
-            Psych.parse_file(file_name)
+        ######################################### Instance Variables ##########################################
+
+        # Constructor for YurlReader Class
+        def initialize(yml_file)
+            @file_name = yml_file
+            @secret_ruby = YurlReader.load_file(yml_file)
         end
         
-        # File Loader For YurlReader Class
+        # Method To Find Nodes With Specific Values
+        def find(query)
+            query_list = YurlReader.process_query_string(query)
+            ret = self.find_nested(query_list)
+            if ret.nil?
+                return "Parameter Not Found At Path - #{query}"
+            end
+            ret
+        end
+
+        ################################ Class Methods #################################
+
+        def self.find_nested(query_array, source_array)
+            ret_val = source_array
+            loop do
+                param = query_array.pop
+                break if ( ret_val.nil? || param.nil?)
+                ret_val = YurlReader.find_internal(param, ret_val)
+            end
+            ret_val
+        end
+
+         # Method To Check If Param Is A Key In The Array
+        def self.find_internal(param, array)
+            return nil unless (array.respond_to?('each'))
+            if(array[param].nil?)
+                return nil
+            else
+                array[param]
+            end
+        end
+        
+        # Method to Parse Query List in Query Array 
+        def self.process_query_string(query_string)
+            puts query_string
+            query_list = query_string.split('/')
+            raise ArgumentError, 'Query String In Wrong Format' unless query_list.respond_to?('each')
+            pp query_list
+            query_list.reverse
+        end
+
+        # Self File Loader For YurlReader Class
+        # Need to put in logic about an empty yaml file
         def self.load_file(yml_file)
-            raise ArgumentError, 'Attempted To Process Null Object' if (yml_file.nil?)
+            raise ArgumentError, 'Attempted To Process Null Object' if yml_file.nil?
             raise ArgumentError, 'Attempted To Process Non YAML File' unless ((File.extname(yml_file) == '.yml') || (File.extname(yml_file) == '.yaml'))
-            
             @file_name = yml_file
-            #@secret_ruby = Psych.parse_file(yml_file).to_ruby
-            @secret_ruby = Psych.methods
-            puts @secret_ruby
+            Psych.parse_file(yml_file).to_ruby
         end
 
         # Loading Yaml From Directly Passed Yaml Text
         def self.load_yaml(yml)
             raise ArgumentError, 'Attempted To Process Null Object' if (yml.nil?)
-
-            @file_name = "Directly Passed Yaml Text"
-            psych_yml = parse(yml)
+            psych_yml = Psych.parse(yml)
             @secret_ruby = psych_yml.to_ruby
         end
 
@@ -40,25 +82,25 @@ module Yurl
             puts "Pretty Print The YAML Dump - #{@file_name}"
             pp @secret_ruby 
         end
+       
+    end
+end
 
-        # Method to Parse Query List in Query Array 
-        def process_query_string(query_string)
-            query_list = query_string.split('/')
-            raise ArgumentError, 'Query String In Wrong Format' unless (query_string.respond_to?('each'))
-            return query_list.reverse
+=begin
+
+        def initialize(yml_file)
+            raise ArgumentError, 'Attempted To Process Null Object' if (yml_file.nil?)
+            raise ArgumentError, 'Attempted To Process Non YAML File' unless ((File.extname(yml_file) == '.yml') || (File.extname(yml_file) == '.yaml'))
+            @file_name = yml_file
+            @secret_ruby = File.open(yml_file)
+            #@secret_ruby = Psych.parse_file(yml_file).to_ruby
+            #@secret_ruby = "Psych.methods"
+            #self.secret_ruby = Psych.parse_file
+            puts @secret_ruby
         end
 
-        # Method To Find Nodes With Specific Values
-        def find(query)
-            query_list = self.process_query_string(query)
-            ret = self.find_nested(query_list)
-            if ret.nil?
-                return "Parameter Not Found At Path - #{query}"
-            end
-            return ret
-        end
 
-        # Method To Check If Param Is A Key In The Array
+            # Method To Check If Param Is A Key In The Array
         def find_internal(param, array)
             return nil unless (array.respond_to?('each'))
             
@@ -67,17 +109,23 @@ module Yurl
             else
                 return array[param]
             end
-        end
 
-        # Method To Cycle Through Query Array And Ruby Object Of The Yaml
+                    # Method To Cycle Through Query Array And Ruby Object Of The Yaml
         def find_nested(query_array)
             ret_val = @secret_ruby
             loop do
                 param = query_array.pop
-                break if((ret_val.nil?) || (param.nil?))
-                ret_val = self.find_internal(param, ret_val)
+                break if ((ret_val.nil?) || (param.nil?))
+                ret_val = YurlReader.find_internal(param, ret_val)
             end
-            return ret_val
+            ret_val
         end
-    end
-end
+
+        def self.find_internal(param, array)
+            return nil unless ( array.respond_to?('each') || !array[param].nil? )
+            
+            array[param]
+
+        end
+        end
+=end
--- spec/yurl_reader_spec.rb
+++ spec/yurl_reader_spec.rb
@@ -1,8 +1,43 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe Yurl::YurlReader do
-  let(:test_val) { Yurl::YurlReader.parse_file("spec/helpers/dummy.yml") }
-  it "loads a yaml file" do
-    expect(test_val.secret_ruby).to be_an_instance_of(Hash)
+  let(:test_val) { Yurl::YurlReader.new('spec/helpers/dummy.yml') }
+  let(:param) { Yurl::YurlReader.process_query_string('Test Nested Secrets/Nested Secret/Super Nested Secret/username') }
+  it 'constructor instantiates instance variables' do
+    expect(test_val.file_name).to be_instance_of(String)
+    expect(test_val.secret_ruby).to respond_to('each')
+  end
+
+  ################### Test Class Level Methods ######################
+
+  it 'can load an yaml file' do
+    test_file = Yurl::YurlReader.load_file('spec/helpers/dummy.yml')
+    expect(test_file).to respond_to('each')
+  end
+
+  # Need to Mock opening non yaml file
+  it 'throws an error when non yaml file passed' do
+    expect(true).to eq(true)
+  end
+
+  it 'parses directly passed yaml' do
+    test_yaml = Yurl::YurlReader.load_yaml("---\n - Test Yaml\n - More Yaml")
+    expect(test_yaml).to respond_to('each')
+  end
+
+  it 'parses string yaml path to param array' do
+    expect(param).to respond_to('each')
+    expect(param.length).to eq(4)
+    expect(param[0]).to eq('username')
+  end
+
+  it 'can search an array for a parameter' do
+    in_arr = Yurl::YurlReader.find_internal('Nonested Secrets', test_val.secret_ruby)
+    out_arr = Yurl::YurlReader.find_internal('Nonested Secret', test_val.secret_ruby)
+    #non_assoc_arr = Yurl::YurlReader.find_internal('username', param)
+
+    expect(in_arr).to be_instance_of(String)
+    expect(out_arr).to eq(nil)
   end
 end
+
